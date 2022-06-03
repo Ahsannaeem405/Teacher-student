7f2560a1bde98beda3c81026ca917a9dc1cd967f
naeemliaqatweb <naeemliaqatweb@gmail.com>
2022-06-03 16:53:28 +0500

    push

--- resources/views/user/layout/footer.blade.php
+++ resources/views/user/layout/footer.blade.php
@@ -44,3 +44,12 @@
         <img src="{{url('/images/dot-shape-primary.svg')}}" alt="Image"/>
     </div>
 </div>
+
+</body>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins)   Order is important -->    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>    
+<!-- Include all compiled plugins (below), or include individual files as needed -->    
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> 
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+</html
