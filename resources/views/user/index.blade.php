8f265027ff494839fffa9f6e35d9b8d16520e245
TassawarAbbas786 <tassawara164@gmail.com>
2022-06-09 10:34:15 +0500

    tassawr abbas

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -78,17 +78,24 @@
                 <button class="btn freeBtn ">Start Free trail</button>
             </div>
         </div>
+        <img class="header-dots-img" src="http://localhost/Teacher-student/public/images/dot-shape-primary.svg" alt="Image">
         <section style="margin-top: 15px;" class="cardbgcolor">
 <div class="container box_b">
 
 <div class="row">
-<div class="col-md-2">
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmGtlD4H_m8lH4C6Kw26faGl1f9DD7-0iCeS_8EJEe_e91Pm9mqNDCj8QqhB9hezpmUdI&usqp=CAU" height="64" width="162" alt="" srcset="">
+<div class="col-md-2 dots-div-height">
+    <div class="search-dots">
+    <span class="dot first-dot-color"></span>
+    <span class="dot second-dot-color"></span>
+    <span class="dot third-dot-color"></span>
+    </div>
+<!-- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmGtlD4H_m8lH4C6Kw26faGl1f9DD7-0iCeS_8EJEe_e91Pm9mqNDCj8QqhB9hezpmUdI&usqp=CAU" height="64" width="162" alt="" srcset=""> -->
 </div>
-<div class="col-md-9">
-    <div class="input-group mt mb">
-        <input type="text" class="form-control" placeholder="Pathon..." aria-describedby="basic-addon2">
-        <span class="input-group-addon" id="basic-addon2"><i class="glyphicon glyphicon-search"></i></span>
+<div class="col-md-9" >
+    <div class="mt mb position">
+    <i class="fa-solid fa-magnifying-glass serach-icon-design"></i>
+        <input type="text" class="input-bg-color text-size" placeholder="Python...">
+       
       </div>
 </div>
 </div>
@@ -102,21 +109,20 @@
         <section class="cardbgcolor">
 
 <div class="container ">
-    <div class="row">
+    <div class="row card-margin">
         <div class="col-md-4 col-lg">
             <div class="card border box_b">
-                <img class="card-img-top" src="{{url('images/black.png')}}" alt="Card image cap">
+                <img class="card-img-top card-img-radius" src="{{url('images/black.png')}}" alt="Card image cap">
                 <div class="card-body">
                   <h5 class="card-title card_heading" >Virtual Teaching</h5>
                   <p class="card-text">Let's Learn Python Fast.</p>
                     <div class="row card_section">
-                        <div class="col-md-3 ">
-                            <i class="fa-regular fa-message-middle"></i>
-
-                        </div>
+                    <div class="col-md-3 mar-top">
+                                <i class="fa-solid fa-message icon_prop message-ml"></i>
+                            </div>
                         <div class="col-md-3">
 
-                            <i class="fa-solid fa-user-group icon_prop" ></i> <span class="greyColor">14</span>
+                        <i class="fa-solid fa-download icon-size"></i></i> <span class="greyColor">14</span>
                         </div>
                         <div class="col-md-5">
                             <i class="fa-solid fa-clock icon_prop"></i> <span class="greyColor">15 minute</span>
@@ -127,13 +133,13 @@
         </div>
             <div class="col-md-4 col-lg">
                 <div class="card border box_b">
-                    <img class="card-img-top" src="{{url('images/black.png')}}" alt="Card image cap">
+                    <img class="card-img-top card-img-radius" src="{{url('images/black.png')}}" alt="Card image cap">
                     <div class="card-body">
                       <h5 class="card-title card_heading" >Virtual Teaching</h5>
                       <p class="card-text">Let's Learn Python Fast.</p>
                         <div class="row card_section">
                             <div class="col-md-3 ">
-                                <i class="fa-solid fa-message icon_prop"></i>
+                                <i class="fa-solid fa-message icon_prop message-ml"></i>
                             </div>
                             <div class="col-md-3">
 
@@ -148,13 +154,13 @@
         </div>
             <div class="col-md-4 col-lg">
                 <div class="card border box_b">
-                    <img class="card-img-top" src="{{url('images/black.png')}}" alt="Card image cap">
+                    <img class="card-img-top card-img-radius" src="{{url('images/black.png')}}" alt="Card image cap">
                     <div class="card-body">
                       <h5 class="card-title card_heading" >Virtual Teaching</h5>
                       <p class="card-text">Let's Learn Python Fast.</p>
                         <div class="row card_section">
                             <div class="col-md-3 ">
-                                <i class="fa-solid fa-message icon_prop"></i>
+                                <i class="fa-solid fa-message icon_prop message-ml"></i>
                             </div>
                             <div class="col-md-3">
 
@@ -175,35 +181,43 @@
 <div class="container">
 <div class="row">
 <div class="col-md-12">
-    <img src="https://image.shutterstock.com/image-photo/business-lady-work-company-manager-260nw-1100223410.jpg" class="img_width" alt="" srcset="">
+    <img src="https://olc-wordpress-assets.s3.amazonaws.com/uploads/2020/03/Online-teaching-class-header-1024x561.png" class="img_width" alt="" srcset="">
 </div>
 
 </div>
 
 </div>
-
+<div class="dots-div">
+<img src="{{url('/images/dot-shape-primary.svg')}}" alt="Image"/>
+<img src="{{url('/images/dot-shape-primary.svg')}}" alt="Image"/>
+<img src="{{url('/images/dot-shape-primary.svg')}}" alt="Image"/>
+</div>
 <div class="container">
     <div class="row">
     <div class="col-md-12 text-center">
-<h1>
+<h1 class="under-line">
 With Virtually Teaching You Can
 </h1>
+
     </div>
 
-    <div class="col-md-12 box_b">
-<div class='row'>
-    <div class='col-md-6'>
-<h3>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error natus provident, expedita, molestiae adipisci vitae perspiciatis at ipsum, repellat earum assumenda ea. Nesciunt vero sapiente voluptates totam incidunt sit eligendi.
-</h3>
-<a href="#" class="btn btn-primary">Learn More</a>
+    <div class="col-sm-12 box_b vistual-margin">
+<div class='virtual-width'>
+    <div class='col-md-5'>
+        <h2 class="margin-virtual-card">Flexible and Convienient</h2>
+<p class="text-design margin-virtual-card">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Error natus provident, expedita, molestiae adipisci vitae perspiciatis at ipsum, repellat earum assumenda ea. Nesciunt vero sapiente voluptates totam incidunt sit eligendi.
+</p>
+<a href="#" class="btn btn-bg-color learn-ml">Learn More</a>
     </div>
-    <div class='col-md-6'>
-        <img src="https://image.shutterstock.com/image-photo/business-lady-work-company-manager-260nw-1100223410.jpg" class="img_width" alt="" srcset="">
+    <div class='col-md-7'>
+        <img src="https://thumbs.dreamstime.com/b/asian-student-boy-video-conference-e-learning-teacher-computer-living-room-home-online-education-internet-social-187311773.jpg" class="img_width-virtual" alt="" srcset="">
     </div>
+   
 </div>
     </div>
-
+    <img class="card-side-dots-img" src="http://localhost/Teacher-student/public/images/dot-shape-primary.svg" alt="Image">
     </div>
 
     </div>
@@ -213,33 +227,76 @@ With Virtually Teaching You Can
 
 
     <div class="container">
-        <div class="row">
+    <div class="row">
+    
+        
         <div class="col-md-12 text-center">
-    <h1>
+    <h1 class="under-line">
     Testimonial
     </h1>
         </div>
     </div>
-<div class="row">
-<div class="col-md-4 col-centered">
+<div class="row testimonial-margin">
+<img class="testimonial-dots" src="http://localhost/Teacher-student/public/images/dot-shape-primary.svg" alt="Image">
+<div class="col-md-3 card-ml">
 
 
     <div class="card border box_b">
-<div class="text-center" style="padding-top: 20px">
-    <img class="img-circle " height="110"  width="110" src="{{url('images/black.png')}}" alt="Card image cap">
-<h3>tile img</h3>
+    <i class="fa-solid fa-comment coment-design"></i>
+    <div class="text-center" style="padding-top: 20px">
+    
+    <img class="img-circle img-margin " height="110"  width="110" src="{{url('images/black.png')}}" alt="Card image cap">
+   
+    <h3>tile img</h3>
 </div>
-        <div class="card-body">
+        <div class="card-body card-body-bg">
+        <div class="star-icon">
 
+    <p class="t_para">Let's Learn Python kdfaj da dfjakd fds jda fdkal fd alkj afja 
+        sdfljad fljads fljads fjds flj Fast.
+
+
+    </p>
+    <div class="star-margin">
+    <i class="glyphicon glyphicon-star star_color"></i>
+<i class="glyphicon glyphicon-star star_color"></i>
+<i class="glyphicon glyphicon-star star_color"></i>
+<i class="glyphicon glyphicon-star"></i>
+<i class="glyphicon glyphicon-star"></i>
+</div>
+</div>
+
+
+            </div>
+      </div>
+
+</div>
+<div class="col-md-3">
+
+
+    <div class="card border box_b">
+    <i class="fa-solid fa-comment coment-design"></i>
+    <div class="text-center" style="padding-top: 20px">
+    
+    <img class="img-circle img-margin " height="110"  width="110" src="{{url('images/black.png')}}" alt="Card image cap">
+   
+    <h3>tile img</h3>
+</div>
+        <div class="card-body card-body-bg">
+        <div class="star-icon">
 
     <p class="t_para">Let's Learn Python kdfaj da dfjakd fds jda fdkal fd alkj afja sdfljad fljads fljads fjds flj Fast.
-<br>
-<i class="glyphicon glyphicon-star star_color"></i>
-<i class="glyphicon glyphicon-star star_color"></i>
-<i class="glyphicon glyphicon-star star_color"></i>
-<i class="glyphicon glyphicon-star"></i>
-<i class="glyphicon glyphicon-star"></i>
+
+
     </p>
+    <div class="star-margin">
+    <i class="glyphicon glyphicon-star star_color"></i>
+<i class="glyphicon glyphicon-star star_color"></i>
+<i class="glyphicon glyphicon-star star_color"></i>
+<i class="glyphicon glyphicon-star"></i>
+<i class="glyphicon glyphicon-star"></i>
+</div>
+</div>
 
 
             </div>
@@ -252,60 +309,6 @@ With Virtually Teaching You Can
 
 
 
-
-
-
-
-<div class="select-sec">
-    <div class="container">
-        <h2>SELECT THE PAYMENT OPTION AND CONTINUE</h2>
-        <div class="row col-middle">
-            <div class="col-md-7 form-col">
-                <div class="row">
-                    <form action="" id="payment_form">
-                        <ul class="payment-form-row">
-                            <li id="border_pay">
-                                <input type="radio" id="html" name="payment_method" value="pay_pal">
-                                <label for="html"><img src="{{url('/images/pay.png')}}" alt="Image"/></label>
-                            </li>
-                            <li id="border_visa">
-                                <input type="radio" id="html" name="payment_method" value="Visa">
-                                <label for="html"><img src="{{url('/images/visanew.png')}}" alt="Image"/></label>
-                            </li>
-                        </ul>
-                        <label for="email" class="mt-5"><h3>PayPal E-mail </h3></label><br>
-                        <input type="email" id="email" name="email">
-                        <input type="submit" value="Pay Now">
-                    </form>
-                    <p>(You will be redirected to official payment gateway page)</p>
-                </div>
-            </div>
-            <div class="col-md-5 order-sum-col">
-                <h2>ORDER SUMMERY</h2>
-                <div class="row basic-sec mb-4">
-                    <div class="col-md-6">
-                        <h3>Basic Plan</h3>
-                    </div>
-                    <div class="col-md-6">
-                         <span>$10 / Month</span>
-                    </div>
-                </div>
-                <h3>Plane Details</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae purus in enim dictum congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer</p>
-                <ul class="footer-link">
-                    <li><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp; Watch your video anytime at your leisure</li>
-                    <li><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp; Get 50 videos per month for $10 a month</li>
-                    <li><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp; Watch videos anytime at your leisure</li>
-                    <li><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp; Share your videos with you social media</li>
-                </ul>
-            </div>
-        </div>
-        <div class="back-to-sec">
-            <h4>Back To Payment Option</h4>
-            <hr>
-        </div>
-    </div>
-</div>
 <div class="next-project">
     <div class="two-img">
         <img src="{{url('/images/dot-shape-primary.svg')}}" alt="Image"/>
