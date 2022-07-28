98d5f253da703c92b97bb4c2f7abf5b695584d7c
zark.abid25 <zarkabid2511@gmail.com>
2022-07-28 10:17:26 +0500

    zark changes

--- resources/views/mainFindCourse.blade.php
+++ resources/views/mainFindCourse.blade.php
@@ -2,6 +2,9 @@
     @foreach($find as $course)
         @php
             $imagePath = explode('.', !is_null($course->course_image) ? $course->course_image : 'do_not_delete.png');
+            $current = now();
+            $created = $course->created_at;
+            $dateDiff = date_diff($current,$created);
         @endphp
 
         <div class="col-md-4 p_cours_padding">
@@ -12,14 +15,27 @@
                     <h5 class="card-title card_heading" >{{ $course->course_name }}</h5>
                     <p class="card-text">{!! $course->course_description !!}</p>
                     <div class="row card_section m-0">
-                        <div class="col-lg-2 mar-top">
-                            <i class="fa-solid fa-message icon_prop message-ml"></i>
+                        <div class="col-lg-12 mar-top">
+                            <div class="col-md-4">
+                                <a href="{{ url('/login') }}">
+                                    <i class="fa-solid fa-message icon_prop message-ml" style="cursor: pointer"></i>
+                                </a>
+                            </div>
+                            <div class="col-md-4">
+                                <i class="fa-solid fa-download icon-size  icon_prop"></i>
+                            </div>
+                            <div class="col-md-4">
+                                <i class="fa-solid fa-clock icon_prop"></i>
+                            </div>
                         </div>
-                        <div class="col-lg-4">
-                            <i class="fa-solid fa-download icon-size  icon_prop"></i></i> <span class="greyColor">14</span>
-                        </div>
-                        <div class="col-lg-6">
-                            <i class="fa-solid fa-clock icon_prop"></i> <span class="greyColor">15 minute</span>
+                        <div class="col-md-12">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <span class="greyColor">{{ ($course->course_dowloads < 1) ? '0' : $course->course_dowloads}}</span>
+                            </div>
+                            <div class="col-md-4">
+                                <span class="greyColor">{{ ($dateDiff->format('%d') > 0) ? $dateDiff->format('%d').' d' : '' }} {{ $dateDiff->format('%i') }} m</span>
+                            </div>
                         </div>
                     </div>
                 </div>
